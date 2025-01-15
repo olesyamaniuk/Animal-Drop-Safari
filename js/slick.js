d4b37c9c43195aae2013bbc0bee8070b42e4cdb3
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-15 16:16:53 +0200

    3

--- js/slick.js
+++ js/slick.js
@@ -9,7 +9,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: true,
+    dots: false,
     centerMode: true,
     cssEase: 'linear',
 
@@ -17,22 +17,7 @@ $(function () {
 });
 
 
-$(function () {
-  $('.reviews-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: true,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.reviews-list-left',
-    nextArrow: '.reviews-list-right',
-  });
-});
+
 
 
 
